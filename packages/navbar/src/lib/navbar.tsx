3c9396b29bbf4a522816b0088890a9543e379867
Giordano Belverato <belveratogiordano@gmail.com>
2025-01-30 16:58:46 +0100

    Feat: Navbar Component

--- packages/navbar/src/lib/navbar.tsx
+++ packages/navbar/src/lib/navbar.tsx
@@ -1,9 +1,26 @@
+import React from 'react';
 import styles from './navbar.module.css';
 
-export function Navbar() {
+interface NavbarProps {
+  title: string;
+  links: { name: string; url: string }[];
+}
+
+export function Navbar({ title, links }: NavbarProps) {
   return (
     <div className={styles['container']}>
-      <h1>Welcome to Navbar!</h1>
+      <h1>{title}</h1>
+      <nav>
+        <ul className={styles['navList']}>
+          {links.map((link, index) => (
+            <li key={index} className={styles['navItem']}>
+              <a href={link.url} className={styles['navLink']}>
+                {link.name}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </div>
   );
 }
