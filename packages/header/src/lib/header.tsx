7daf2762c274028347ac872dc3fd44da053109c3
Giordano Belverato <belveratogiordano@gmail.com>
2025-02-04 11:21:38 +0100

    Feat: Header Component

--- packages/header/src/lib/header.tsx
+++ packages/header/src/lib/header.tsx
@@ -2,9 +2,16 @@ import styles from './header.module.css';
 
 export function Header() {
   return (
-    <div className={styles['container']}>
+    <header className={styles['container']}>
       <h1>Welcome to Header!</h1>
-    </div>
+      <nav>
+        <ul>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
   );
 }
 
