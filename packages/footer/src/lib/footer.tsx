c7a0bff6e74ba32294bed4907551696eb0a1eb34
Giordano Belverato <belveratogiordano@gmail.com>
2025-02-04 13:46:45 +0100

    Feat: Footer Component

--- packages/footer/src/lib/footer.tsx
+++ packages/footer/src/lib/footer.tsx
@@ -2,9 +2,16 @@ import styles from './footer.module.css';
 
 export function Footer() {
   return (
-    <div className={styles['container']}>
+    <footer className={styles['container']}>
       <h1>Welcome to Footer!</h1>
-    </div>
+      <nav>
+        <ul>
+          <li><a href="#privacy">Privacy Policy</a></li>
+          <li><a href="#terms">Terms of Service</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
+    </footer>
   );
 }
 
